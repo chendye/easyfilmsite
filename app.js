01ecbe8787fedcdc2b9af8c0ed98b51bbe6dc9c0
Si Chen <970831768@qq.com>
2017-09-14 23:59:47 +0800

    增加timestamp 及其相关操作

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.listen(port, '127.0.0.1');
 // index page
 app.get('/', function(req, res) {
   // 查询所有数据内容
-  query('select * from moive_data', [], function(error, results, fields) {
+  query('select * from moive_data order by intime', [], function(error, results, fields) {
     if (error) {
       console.log(error);
     } else {
@@ -57,7 +57,7 @@ app.get('/moive/:id', function(req, res) {
 
 // list page
 app.get('/admin/list', function(req, res) {
-  query('select * from moive_data', [], function(error, results, fields) {
+  query('select * from moive_data order by intime', [], function(error, results, fields) {
     if (error) {
       console.log(error);
     } else {
@@ -89,18 +89,27 @@ app.get('/admin/moive', function(req, res) {
 
 // 添加数据
 app.post('/admin/add', (req, res) => {
-  let [title, doctor, country, year, poster, language, flash, summary] = [req.body.title, req.body.doctor, req.body.country, req.body.year, req.body.poster, req.body.language, req.body.flash, req.body.summary];
-  // 注意添加char test类型的字符串时 需要加''哦
-  query(`insert into moive_data values (null,'${title}','${doctor}','${country}',${year},'${poster}','${language}','${flash}','${summary}')`, [], function(error, results, fields) {
+  let [_title, _doctor, _country, _year, _poster, _language, _flash, _summary] = [req.body.title, req.body.doctor, req.body.country, req.body.year, req.body.poster, req.body.language, req.body.flash, req.body.summary];
+
+  query(`select * from moive_data where title = '${_title}'`, function(error, results, fields) {
     if (error) {
       console.log(error);
-    } else {
-      console.log('add: ', results);
-      res.send({
-        status: 200,
-        msg: "add success",
-        url: "/admin/list"
+    } else if (!results.length) {
+      // 注意添加char test类型的字符串时 需要加''哦
+      query(`insert into moive_data(title, doctor,country,year,poster,language,flash,summary) values ('${_title}','${_doctor}','${_country}',${_year},'${_poster}','${_language}','${_flash}','${_summary}')`, [], function(error, results, fields) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('add: ', results);
+          res.json({
+            status: 200,
+            msg: "add success",
+            url: "/admin/list"
+          });
+        }
       });
+    } else {
+      console.log('影片已存在');
     }
   });
 });
@@ -115,7 +124,7 @@ app.delete('/admin/list', function(req, res) {
     } else {
       console.log('delete: ', results);
       // 刷新页面，重定向到本页面
-      res.send({
+      res.json({
         status: 200,
         msg: "delete success",
         url: "/"
@@ -139,6 +148,7 @@ app.get('/admin/update/:id', function(req, res) {
     }
   });
 });
+
 // 更新数据
 app.put('/admin/add', function(req, res) {
   let id = req.body.id;
@@ -149,7 +159,7 @@ app.put('/admin/add', function(req, res) {
       console.log(error);
     } else {
       console.log('update: ' + results);
-      res.send({
+      res.json({
         status: 200,
         msg: "update success",
         url: "/"
